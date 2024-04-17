0ee09f80480293e8f47e381218fef305590c5667
duanchaoman <429669030@qq.com>
2024-04-17 16:49:21 +0800

    SmartPytest

--- SmartApi/LoginApi.py
+++ SmartApi/LoginApi.py
@@ -23,9 +23,12 @@ def login(userAccount,password):
     login_headers={'Content-Type': 'application/json'}
     login_url = "http://" + config.setuip() +'/smartSecurityAPI/userLogin/login'
     res=requests.post(url=login_url, headers=login_headers, data=payload).json()
+    print(res)
     token=res.get('result').get('token')
     guestCode=res.get('result').get('user').get('guestCode')
     headers = {'Content-Type': 'application/json','token':token,'guestCode':guestCode}
     return res,headers
 
 
+if __name__ == '__main__':
+    login('5229455','Admin@123')
